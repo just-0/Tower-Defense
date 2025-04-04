4a97c75c130a5907a43f4c73679ad2df65804dd5
just-0 <justoperez8910@ucsp.edu.pe>
2025-04-04 02:36:48 -0500

    First commit with MediaPipe Camera Interface

---
+++ Assets/Scripts/WebSocket/GestureReceiver.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using NativeWebSocket;
+using System.Text;
+
+public class GestureReceiver : MonoBehaviour
+{
+    [SerializeField] private RawImage gestureImage;
+
+    private WebSocket websocket;
+    private Texture2D receivedTexture;
+
+    // Clase interna solo para representar los datos
+    [Serializable]
+    private class GestureData
+    {
+        public int fingers;
+        public string image;
+    }
+
+    async void Start()
+    {
+        websocket = new WebSocket("ws://localhost:8765");
+
+        websocket.OnMessage += (bytes) =>
+        {
+            string json = Encoding.UTF8.GetString(bytes);
+            GestureData data = JsonUtility.FromJson<GestureData>(json);
+            byte[] imageBytes = Convert.FromBase64String(data.image);
+
+            if (receivedTexture == null)
+                receivedTexture = new Texture2D(2, 2);
+
+            receivedTexture.LoadImage(imageBytes);
+            gestureImage.texture = receivedTexture;
+        };
+
+        await websocket.Connect();
+    }
+
+    void Update()
+    {
+#if !UNITY_WEBGL || UNITY_EDITOR
+        websocket.DispatchMessageQueue();
+#endif
+    }
+
+    private async void OnApplicationQuit()
+    {
+        await websocket.Close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Tower Defense
+
+Este es un juego de **Tower Defense** desarrollado con Unity.
+
+## Descripción
+Tower Defense Clasico con una diferente manipulacion del juego mediante camaras y deteccion de objetos en tiempo real.
+
+## Requisitos
+
+- Unity 6
+- Sistema operativo: Windows o Linux
+- IDE recomendado: Visual Studio
+
+## Instalación
+
+1. Clona el repositorio en tu máquina local:
+
+   ```bash
+   git clone https://github.com/just-0/Tower-Defense.git
+
